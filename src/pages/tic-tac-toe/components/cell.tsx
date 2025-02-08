cc8a76adde7e594ba4d33670bbda2015ff2bf113
wnsrl7250 <wnsrl7250@naver.com>
2025-02-08 19:25:45 +0900

    틱택토 게임 구현
    - 자식 요소가 있을 경우 클릭 비활성화

--- src/pages/tic-tac-toe/components/cell.tsx
+++ src/pages/tic-tac-toe/components/cell.tsx
@@ -5,19 +5,25 @@ type CellProps = Omit<React.ComponentProps<'button'>, 'onClick'> & {
 };
 
 function Cell({ children, className = '', onPlay, ...restProps }: CellProps) {
+  const hasChildren = !!children;
+
   const handlePlay = () => {
+    if (hasChildren) return;
+
     onPlay?.(); // onPlay가 정의되어 있을 때만 호출
   };
 
   return (
     <button
       type="button"
+      aria-disabled={hasChildren}
       className={tm(
         'cursor-pointer',
         'size-20 border rounded-md',
         'text-2xl font-semibold',
         'border-black/50',
-        'hover:border-black hover:bg-slate-100/60',
+        { 'hover:border-black hover:bg-slate-100/60': !hasChildren },
+        { 'cursor-not-allowed': hasChildren },
         className
       )}
       onClick={handlePlay}
